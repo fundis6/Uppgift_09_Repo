eea01cafb1eb529247f36273861ea82bf7425dba
Oscar Fundberg <oscar.fundberg@hotmail.com>
2021-01-25 14:14:57 +0100

    The choice of rock/paper/scissors
    The menu for the players choice is created with a spelling/wrong answer check as well

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace Uppgift_09_Repo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string choice;
+
+            Console.WriteLine("This is a game of rock paper scissors between you and the computer!");
+            System.Console.Write("Write your choice between the three options: ");
+            choice = Console.ReadLine();
+
+            while (choice.ToLower() != "rock" && choice.ToLower() != "paper" && choice.ToLower() != "scissors")
+            {
+                System.Console.WriteLine("Your answer was invalid, try again and make sure you spelled it right. ");
+                choice = Console.ReadLine();    
+            }
 
             Console.ReadKey();
         }
